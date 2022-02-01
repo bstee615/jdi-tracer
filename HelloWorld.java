20874e930f770c92d763c84f53d4dff47bf053a3
bstee615 <benjaminjsteenhoek@gmail.com>
2022-02-01 15:34:31 -0600

    Initial commit.

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * Debug this program with JDI & read all local variables.
+ * 
+ * @author ravik
+ *
+ */
+public class HelloWorld {
+ 
+	public static void main(String[] args) {
+		String helloWorld = "Hello World. ";
+ 
+		String welcome = "Welcome to Its All Binary !";
+ 
+		String greeting = helloWorld + welcome;
+ 
+		System.out.println("Hi Everyone, " + greeting);// Put a break point at this line.
+ 
+	}
+ 
+}
